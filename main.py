312527d50d7043d5b2cac9385edc4e805f65ec58
ankzrwt <rawat.ankita1998@gmail.com>
2023-04-19 14:07:32 +0530

    commit branch 2

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ transactionService.addMoney('ank1234567890',700)
 transactionService.withdrawl('ank1234567890',100)
 transactionService.checkBalance('ank1234567890')
 transactionService.transferMoney('ank1234567890','gau456661610',1000)
+
+# changes for branch 2
+
 transactionService.checkBalance('ank1234567890')
 transactionService.checkBalance('gau9456661610')
 
